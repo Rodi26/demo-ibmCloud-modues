b98b2ab64e903dc83966d5fac21bb534569969e3
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-06-01 18:04:48 +0200

    default provider

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.13"
   required_providers {
     ibm = {
-      source = "app.terraform.io/rodolphefontaine-demo/ibm"
+      source = "IBM-Cloud/ibm"
     }
   }
 }
